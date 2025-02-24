92ab4a3832a9b9b15cbd592955fcddf337d8a71f
Théophile Delmas <contact@theophile.world>
2025-02-24 10:33:48 +0100

    Multi line description

--- pdfinvoicer/invoicer.go
+++ pdfinvoicer/invoicer.go
@@ -456,7 +456,7 @@ func (i *Invoice) GeneratePDF(outputPath string) error {
 	for _, item := range i.Items {
 		itemTotalWithoutVAT := item.Quantity * item.UnitPrice
 		itemTotal := itemTotalWithoutVAT + item.VATRate*itemTotalWithoutVAT
-		pdf.CellFormat(cols[0], 7, item.Description, "1", 0, "L", false, 0, "")
+		pdf.MultiCell(40, 7, item.Description, "1", "L", false)
 		pdf.CellFormat(cols[1], 7, fmt.Sprintf("%.2f", item.Quantity), "1", 0, "C", false, 0, "")
 		pdf.CellFormat(cols[2], 7, formatCurrency(item.UnitPrice), "1", 0, "R", false, 0, "")
 		pdf.CellFormat(cols[3], 7, fmt.Sprintf("%.0f%%", item.VATRate*100), "1", 0, "C", false, 0, "")
